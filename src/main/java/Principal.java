764c27c5b79034b2ba76c97b26472f4736b94391
dcris <dcristine.estudos@gmail.com>
2026-03-19 11:13:13 -0300

    fix: Comentario de linhas.

--- src/main/java/Principal.java
+++ src/main/java/Principal.java
@@ -6,12 +6,15 @@ import javax.swing.JOptionPane;
 
 
 public class Principal {
-
+//variaveis
     public static void main(String[] args) {
         double base, altura, area;
+        //entrada
         base = Double.parseDouble(JOptionPane.showInputDialog("Digite a base"));
          altura = Double.parseDouble(JOptionPane.showInputDialog("Digite base"));
+         //processamento
           area = (base * altura)/ 2.0;
+          //saida
                   JOptionPane.showMessageDialog(null, "A area é: " + area);
     }
 }
